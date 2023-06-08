2f5b754d932d4a5d862bd7039e870d0c2a482598
EtienneCou <coudert.etienne@gmail.com>
2023-06-08 11:33:32 +0200

    corect conflict

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
 import { Col, Container, Row } from 'react-bootstrap';
 import './App.scss';
 import HomepageInscription from '../HomepageInscription/HomepageInscription';
+import RecipeCard from '../RecipeCard/RecipeCard';
 
 function App() {
   return (
     <Container>
       <Row>
         <Col>
-          <p>Lu</p>
+          <RecipeCard />
         </Col>
       </Row>
       <HomepageInscription />
