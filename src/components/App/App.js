9e373eb88c10aa84947eda90f5ff550d7b13c9ab
EtienneCou <coudert.etienne@gmail.com>
2023-06-09 13:25:01 +0200

    add faq and route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,27 @@
-import { Col, Container, Row } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+
 import Header from '../Header/Header';
 import Recipes from '../Recipes/Recipes';
 import HomepageInscription from '../HomepageInscription/HomepageInscription';
 import Loader from '../Loader/Loader';
+import Faq from '../Faq/Faq';
 
 function App() {
   return (
     <Container fluid className="App">
       <Header />
-      <Row>
-        <Col>
-          <Recipes />
-        </Col>
-      </Row>
-      <HomepageInscription />
+      <Routes>
+        <Route path="/" element={<HomepageInscription />} />
+        <Route
+          path="/home"
+          element={
+            <Recipes />
+            }
+        />
+        <Route path="/faq" element={<Faq />} />
+      </Routes>
       <Loader />
     </Container>
   );
