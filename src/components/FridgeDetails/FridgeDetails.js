57edb5a27e195a8acde398d10e9dac22db384c22
Clément Jamgotchian <112162138+Clanclem@users.noreply.github.com>
2023-06-15 17:19:45 +0200

    starting fridge page

--- src/components/FridgeDetails/FridgeDetails.js
+++ src/components/FridgeDetails/FridgeDetails.js
@@ -1,9 +1,44 @@
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 import './FridgeDetails.scss';
+import fleche from '../../assets/images/fleche.png';
+import ModalFridge from './ModalFridge/ModalFridge';
 
 function FridgeDetails() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <div>
-      <p>test</p>
+    <div className="FridgeDetails">
+      <div className="FridgeDetails-titles">
+        <p>Ingrédients</p>
+        <p>Quantité</p>
+        <p>Unité</p>
+        <p>Supprimer</p>
+      </div>
+      <div className="FridgeDetails-form">
+        <img
+          className="FridgeDetails-img"
+          src={fleche}
+          alt="Flèche vers la droite"
+        />
+        <p className="FridgeDetails-ingredient">Tomates</p>
+        <input className="FridgeDetails-input" />
+        <p className="FridgeDetails-unit">cl</p>
+        <button
+          type="button"
+          className="FridgeDetails-button"
+          aria-label="croix de fermeture"
+        />
+      </div>
+      <div className="FridgeDetails-add">
+        <Button variant="outline-primary" onClick={handleShow}>
+          Ajouter
+        </Button>
+        <Button variant="success">Voir plus</Button>
+      </div>
+      <ModalFridge handleClose={handleClose} show={show} />
     </div>
   );
 }
--- src/pages/Fridge/Fridge.js
+++ src/pages/Fridge/Fridge.js
@@ -2,8 +2,10 @@ import './Fridge.scss';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import fridgeLogo from '../../assets/images/frigo.png';
 import closetLogo from '../../assets/images/placard.png';
+import FridgeDetails from '../../components/FridgeDetails/FridgeDetails';
 
 function Fridge() {
   return (
@@ -18,6 +20,7 @@ function Fridge() {
               className="Fridge-img"
             />
           </h3>
+          <FridgeDetails />
         </Col>
         <Col className="Fridge-closet">
           <h3 className="Fridge-Title">
@@ -28,8 +31,12 @@ function Fridge() {
               className="Fridge-img"
             />
           </h3>
+          <FridgeDetails />
         </Col>
       </Row>
+      <Button className="Fridge-button" variant="primary" size="lg">
+        Block level button
+      </Button>
     </Container>
   );
 }
