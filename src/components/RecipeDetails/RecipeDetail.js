f30a0488056f4d52b1f5ed950998bf06867c1b88
Leslie Cordier <lesliecordier83@gmail.com>
2023-06-22 16:44:51 +0200

    Lazyloading pictures

--- src/components/RecipeDetails/RecipeDetail.js
+++ src/components/RecipeDetails/RecipeDetail.js
@@ -5,6 +5,7 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { getDifficultyLabel, getStars } from '../../utils/formatRecipeData';
 
 import cuisine from '../../assets/cuisine.png';
@@ -21,6 +22,8 @@ import AxiosPrivate from '../../utils/AxiosPrivate';
 import AxiosPublic from '../../utils/AxiosPublic';
 import Rating from './Rating/Rating';
 import CommentsCarousel from './CommentsCarousel/CommentsCarousel';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import Loader from '../Loader/Loader';
 
 // If user is logged in, we show the cart icon
 function CartIcon({ addToList, isFavorite }) {
@@ -83,116 +86,121 @@ function RecipeDetails() {
         console.log('erreur dans recette detaillé');
       });
   }, []);
-  return (
 
+  return (
     <section className="recipeDetails">
       <Header className="recipeDetails-layout" style={{ display: 'none' }} />
       <Menuphone className="recipeDetails-layout" />
-      <section
-        className="recipeDetails-header"
-      >
-        <div className="recipeDetails-header-imgAndButton">
-          <img
-            src={getPicture(recipe)}
-            alt="la recette"
-            className="recipeDetails-header-image"
-          />
-          <button type="button" className="recipeDetails-header-cancelButton" onClick={handleClick}>
-            <p className="recipeDetails-header-cancelButton-image">&#10005;</p>
-          </button>
-          <CartIcon
-            isLoggedIn={isLoggedIn}
-            addToList={() => {
-              addToList(idRecette);
-            }}
-            isFavorite={cartOn}
-          />
-          <FavoriteIcon
-            recipe={recipe}
-          />
-        </div>
-        <div className="recipeDetails-header-container">
-          <h1 className="recipeDetails-header-title">{recipe.title}</h1>
-          <div className="recipeDetails-header-rating">{getStars(recipe.rating)}</div>
-          <p>
-            Niveau :
-            {' '}
-            {getDifficultyLabel(recipe.difficulty)}
-          </p>
-
-          <section className="recipeDetails-preparation">
-            <div className="recipeDetails-preparation-time">
-              <img src={cuisine} alt="icone de preparation" className="recipeDetails-preparation-image" />
-              <h3 className="recipeDetails-preparation-title">
-                Préparation
-                {' '}
-                <span className="recipeDetails-preparation-title-quantity">
-                  {recipe.cookingDuration}
-                  {' '}
-                  min
-                </span>
-              </h3>
+      {recipe.title ? (
+        <>
+          <section
+            className="recipeDetails-header"
+          >
+            <div className="recipeDetails-header-imgAndButton">
+              <LazyLoadImage
+                className="recipeDetails-header-image"
+                src={getPicture(recipe)}
+                effect="blur"
+              />
+              <button type="button" className="recipeDetails-header-cancelButton" onClick={handleClick}>
+                <p className="recipeDetails-header-cancelButton-image">&#10005;</p>
+              </button>
+              <CartIcon
+                isLoggedIn={isLoggedIn}
+                addToList={() => {
+                  addToList(idRecette);
+                }}
+                isFavorite={cartOn}
+              />
+              <FavoriteIcon
+                recipe={recipe}
+              />
             </div>
-            <div className="recipeDetails-preparation-cuisson">
-              <img src={cuisson} alt="icone de cuisson" className="recipeDetails-preparation-image" />
-              <h3 className="recipeDetails-preparation-title">
-                Cuisson
+            <div className="recipeDetails-header-container">
+              <h1 className="recipeDetails-header-title">{recipe.title}</h1>
+              <div className="recipeDetails-header-rating">{getStars(recipe.rating)}</div>
+              <p>
+                Niveau :
                 {' '}
-                <span className="recipeDetails-preparation-title-quantity">
-                  {recipe.setupDuration}
-                  {' '}
-                  min
-                </span>
+                {getDifficultyLabel(recipe.difficulty)}
+              </p>
 
-              </h3>
-            </div>
-          </section>
-        </div>
-      </section>
+              <section className="recipeDetails-preparation">
+                <div className="recipeDetails-preparation-time">
+                  <img src={cuisine} alt="icone de preparation" className="recipeDetails-preparation-image" />
+                  <h3 className="recipeDetails-preparation-title">
+                    Préparation
+                    {' '}
+                    <span className="recipeDetails-preparation-title-quantity">
+                      {recipe.cookingDuration}
+                      {' '}
+                      min
+                    </span>
+                  </h3>
+                </div>
+                <div className="recipeDetails-preparation-cuisson">
+                  <img src={cuisson} alt="icone de cuisson" className="recipeDetails-preparation-image" />
+                  <h3 className="recipeDetails-preparation-title">
+                    Cuisson
+                    {' '}
+                    <span className="recipeDetails-preparation-title-quantity">
+                      {recipe.setupDuration}
+                      {' '}
+                      min
+                    </span>
 
-      <section className="recipeDetails-ingredients-container">
-        <section className="recipeDetails-ingredients">
-          <h2 className="recipeDetails-ingredients-titles">
-            Ingredients pour
-            {' '}
-            {recipe.portions}
-            {' '}
-            personnes
-          </h2>
-          <div className="recipeDetails-ingredients-ingredient">
-            {containsIngredients.map((item) => (
-              <section key={item.ingredient.id} className="recipeDetails-ingredients-ingredient-item">
-                <img className="recipeDetails-ingredients-ingredient-item-image" src={vegetables} alt="icone de l'ingrédient" />
-                <div className="recipeDetails-ingredients-ingredient-item-container">
-                  <p className="recipeDetails-ingredients-ingredient-item-name">{item.ingredient.name}</p>
-                  <p className="recipeDetails-ingredients-ingredient-item-name-quantity">
-                    {item.quantity}
-                    {item.ingredient.unit}
-                  </p>
+                  </h3>
                 </div>
               </section>
-            ))}
+            </div>
+          </section>
 
-          </div>
-        </section>
-        <section className="recipeDetails-recette">
-          <h2 className="recipeDetails-recette-titles">Recette</h2>
-          <div
-            className="recipeDetails-recette-step"
+          <section className="recipeDetails-ingredients-container">
+            <section className="recipeDetails-ingredients">
+              <h2 className="recipeDetails-ingredients-titles">
+                Ingredients pour
+                {' '}
+                {recipe.portions}
+                {' '}
+                personnes
+              </h2>
+              <div className="recipeDetails-ingredients-ingredient">
+                {containsIngredients.map((item) => (
+                  <section key={item.ingredient.id} className="recipeDetails-ingredients-ingredient-item">
+                    <img className="recipeDetails-ingredients-ingredient-item-image" src={vegetables} alt="icone de l'ingrédient" />
+                    <div className="recipeDetails-ingredients-ingredient-item-container">
+                      <p className="recipeDetails-ingredients-ingredient-item-name">{item.ingredient.name}</p>
+                      <p className="recipeDetails-ingredients-ingredient-item-name-quantity">
+                        {item.quantity}
+                        {item.ingredient.unit}
+                      </p>
+                    </div>
+                  </section>
+                ))}
+
+              </div>
+            </section>
+            <section className="recipeDetails-recette">
+              <h2 className="recipeDetails-recette-titles">Recette</h2>
+              <div
+                className="recipeDetails-recette-step"
             // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: steps,
-            }}
-          />
-        </section>
-      </section>
-      {isLoggedIn && (
-        <section className="recipeDetails-comments">
-          {recipe.comments
+                dangerouslySetInnerHTML={{
+                  __html: steps,
+                }}
+              />
+            </section>
+          </section>
+          {isLoggedIn && (
+          <section className="recipeDetails-comments">
+            {recipe.comments
           && <CommentsCarousel comments={recipe.comments} />}
-          <Rating recipe={idRecette} />
-        </section>
-      )}
+            <Rating recipe={idRecette} />
+          </section>
+          )}
+        </>
+      )
+        : (<Loader />)}
       <Footer className="recipeDetails-layout" />
     </section>
   );
